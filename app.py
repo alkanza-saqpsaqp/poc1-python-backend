d573b32806fe475df69029b14e6e0298cad1b4ed
Saúl Quintero <saul.quintero@alkanza.us>
2019-07-18 08:13:31 -0500

    Added title

--- app.py
+++ app.py
@@ -4,15 +4,29 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 PORT_NUMBER = 8080
 
+landing_page = """
+<!doctype html>
+<html lang=en>
+<head>
+<meta charset=utf-8>
+<title>Alkanza</title>
+</head>
+<body>
+<h1>Título</h1>
+<h3>branch001</h3>
+</body>
+</html>
+"""
+
 class MyHandler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     """Handler for GET requests"""
     self.send_response(200)
-    self.send_header('Content-type','image/png')
+    self.send_header("Content-type", "text/html")
     self.end_headers()
-    with open('logo.png', 'rb') as f:
-      self.wfile.write(f.read())
+    content = landing_page.encode('utf-8')
+    self.wfile.write(content)
 
 try:
   server = HTTPServer(('', PORT_NUMBER), MyHandler)
